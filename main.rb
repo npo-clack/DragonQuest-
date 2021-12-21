7a8635ff02dea3c98b302bf7512e29f186b8873d
furuike <cactus0411.d@gmail.com>
2021-12-21 23:13:45 +0900

    Update main.rb
    ゲーム性が出るように修正（相手が攻撃しているときは防御したい）

--- main.rb
+++ main.rb
@@ -4,6 +4,6 @@ require './brave'
 require './monster'
 require './games_controller'
 games_controller = GamesController.new
-merioeru = Brave.new(name: "メリオエル", hp: 5000, offense: 200, defense: 100)
-sukumono = Monster.new(name: "巣食うもの", hp: 2500, offense: 200, defense: 100)
+merioeru = Brave.new(name: "メリオエル", hp: 2500, offense: 500, defense: 500)
+sukumono = Monster.new(name: "巣食うもの", hp: 2500, offense: 800, defense: 200)
 games_controller.battle(brave: merioeru, monster: sukumono)
